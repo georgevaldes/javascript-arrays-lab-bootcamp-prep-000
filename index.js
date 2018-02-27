3dc5b74a60eb96b247de52c70a67f45362ab7210
George Valdes <gv2186@gmail.com>
2018-02-27 02:13:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var newKittens = kittens.slice(kittens.length - 1)
+  var newKittens = kittens.slice(0, kittens.length - 1)
   
   console.log("HERE IS THE ANSWER ", newKittens)
   
