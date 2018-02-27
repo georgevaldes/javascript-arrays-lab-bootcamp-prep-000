04938852a1118c4ab4db6a12e7ab2a76f4a94597
George Valdes <gv2186@gmail.com>
2018-02-27 02:13:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+kittens = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name)
+}
+
+function appendKitten(name){
+  const newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name){
+  const newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(kittens.length - 1)
+  
+  console.log("HERE IS THE ANSWER ", newKittens)
+  
+  return newKittens
+}
+
+console.log("HERE IS THE LIST ",kittens)
+console.log(removeLastKitten())
+
+
